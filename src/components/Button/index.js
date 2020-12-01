628785d709116e977bdb13fd378a959fd32a6b38
NatNatali <natalibalasanyan2@gmail.com>
2020-12-01 10:29:07 +0400

    feat(SignIn): Create button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.scss'
+
+const Button = () => {
+    return <div className='button'>
+        <button>SUBMIT</button>
+    </div>
+}
+
+export default Button;
--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -2,42 +2,47 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 import { Link } from "react-router-dom";
 import Container from '../Container';
+import Button from "../Button";
 import './index.scss'
 
 const SignIn = () => {
     const { register, handleSubmit, watch, errors } = useForm()
     const onSubmit = data => { console.log(data) }
     return <Container>
-            <div className='l-content'>
-                <div className='l-h1'><h1>Sign In</h1></div>
-                <div className='l-question'>Don't have an account?  <Link to="/sign-up"><span>Sign Up</span></Link></div>
+            <div>
+                <div className='l-texts'>
+                    <div className='l-h1'>Sign In</div>
+                    <div className='l-question'>Don't have an account?  <Link to="/sign-up"><span>Sign Up</span></Link></div>
+                </div>
                 <div className='l-form-content'>
                     <form onSubmit={handleSubmit(onSubmit)}>
-                       <div>
+                       <div className='email-input'>
                            <label htmlFor="">Email</label>
                            <input
                                name="email"
                                type='email'
                                ref={register({
-                                   required: "You must specify a email",
+                                   required: "You must specify an email",
                                })}
                            />
                        </div>
                         {errors.email && <p className='l-error'>{errors.email.message}</p>}
-                        <label htmlFor="">Password</label>
-                        <input
-                            name="password"
-                            type='password'
-                            ref={register({
-                                required: "You must specify a password",
-                                minLength: {
-                                    value: 8,
-                                    message: "Password must have at least 8 characters"
-                                }})
-                            }
-                        />
+                        <div className='pass-input'>
+                            <label htmlFor="">Password</label>
+                            <input
+                                name="password"
+                                type='password'
+                                ref={register({
+                                    required: "You must specify a password",
+                                    minLength: {
+                                        value: 8,
+                                        message: "Password must have at least 8 characters"
+                                    }})
+                                }
+                            />
+                        </div>
                         {errors.password && <p className='l-error'>{errors.password.message}</p>}
-                        <div className='l-button'><button>SUMBIT</button></div>
+                        <Button />
                     </form>
 
                 </div>
