4932e6f24cd6c95508aea390f7bd500a454f5b72
NatNatali <natalibalasanyan2@gmail.com>
2020-12-18 18:37:08 +0400

    fix(Auth): Add media query

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link, useLocation} from "react-router-dom";
 import mainLogo from  '../../images/logo_transparent 1.png'
 import './index.scss';
+import { UserOutlined } from '@ant-design/icons';
 
 const Header = () => {
 
@@ -20,14 +21,6 @@ const Header = () => {
             name: 'Մեկնաբանություն',
             link: '/comments',
             className: 'menu-item menu-item-color'
-        },{
-            name: 'Մուտք',
-            link: '/sign-in',
-            className: 'menu-item menu-item-color'
-        },{
-            name: 'Գրանցում',
-            link: '/sign-up',
-            className: 'menu-item menu-item-color'
         },
     ];
 
@@ -39,6 +32,7 @@ const Header = () => {
                         return <div className={ location.pathname === item.link ? item.className : 'menu-item'} key={index}><Link to={item.link}>{item.name}</Link></div>
                     })
                 }
+                <Link to='/sign-in'><UserOutlined className={ location.pathname === '/sign-in' || location.pathname === '/sign-up' ? 'menu-item menu-item-color' : 'menu-item'}/></Link>
             </div>
         </div>
     );
